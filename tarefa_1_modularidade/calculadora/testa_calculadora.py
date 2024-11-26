7d6287b0165cf7d4b3728e2d52c60c14ade2ec39
Caik Franco <128155208+caikfra@users.noreply.github.com>
2024-11-26 20:36:51 +0000

    update calculadora.py

---
+++ tarefa_1_modularidade/calculadora/testa_calculadora.py
@@ -0,0 +1,14 @@
+import calculadora as calc
+
+def main():
+    print(calc.soma(5,5))
+    print(calc.soma(5,))
+    print(calc.sub(5,5))
+    print(calc.sub(5,))
+    print(calc.mul(5,5))
+    print(calc.mul(5,))
+    print(calc.div(5,5))
+    print(calc.div(5,))
+
+if __name__ == "__main__":
+    main()
--- tarefa_1_modularidade/testa_calculadora.py
+++
@@ -1,9 +0,0 @@
-import calculadora as calc
-print(calc.soma(5,5))
-print(calc.soma(5,))
-print(calc.sub(5,5))
-print(calc.sub(5,))
-print(calc.mul(5,5))
-print(calc.mul(5,))
-print(calc.div(5,5))
-print(calc.div(5,))
