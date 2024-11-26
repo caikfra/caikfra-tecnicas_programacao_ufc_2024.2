ce003dadfc539bcf8430a33a030269d854b5fc72
Caik Franco <128155208+caikfra@users.noreply.github.com>
2024-11-26 20:37:49 +0000

    adicionando conversores

---
+++ tarefa_1_modularidade/conversores/conversores.py
@@ -0,0 +1,20 @@
+def celsius_para_fahrenheit(celsius: float) -> float:
+    return (celsius * 9/5) + 32
+
+def fahrenheit_para_celsius(fahrenheit: float) -> float:
+    return (fahrenheit - 32) * 5/9
+
+def metro_para_pes(metros: float) -> float:
+    return metros * 3.28084
+
+def pes_para_metro(pes: float) -> float:
+    return pes / 3.28084
+
+def quilometro_para_milhas(quilometros: float) -> float:
+    return quilometros * 0.621371
+
+def milhas_para_quilometro(milhas: float) -> float:
+    return milhas / 0.621371
+
+def dia_para_horas(dias: float) -> float:
+    return dias * 24
---
+++ tarefa_1_modularidade/conversores/teste_conveesores.py
@@ -0,0 +1,13 @@
+import conversores as c
+
+def main():
+    print(c.celsius_para_fahrenheit(0))  
+    print(c.fahrenheit_para_celsius(32)) 
+    print(c.metro_para_pes(1))  
+    print(c.pes_para_metro(3.28084))  
+    print(c.quilometro_para_milhas(1))  
+    print(c.milhas_para_quilometro(1))  
+    print(c.dia_para_horas(1))  
+
+if __name__ == "__main__":
+    main()
